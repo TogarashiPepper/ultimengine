a750525307af2d87fc11a8d78753a480adf44be1
TogarashiPepper <83902606+TogarashiPepper@users.noreply.github.com>
2025-05-31 16:05:59 -0700

    add board formatting & random move eng

--- src/board.rs
+++ src/board.rs
@@ -8,42 +8,93 @@ pub enum Slot {
     O,
 }
 
+impl Slot {
+    fn to_chr(self) -> char {
+        match self {
+            Slot::Empty => ' ',
+            Slot::X => 'X',
+            Slot::O => 'O',
+        }
+    }
+}
+
 impl Display for Slot {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                Slot::Empty => ' ',
-                Slot::X => 'X',
-                Slot::O => 'O',
-            }
-        )
+        write!(f, "{}", self.to_chr())
     }
 }
 
 pub type Board = [Slot; 9];
 
+fn tranpose(board: &Board) -> Board {
+    let mut new = *board;
+
+    new.swap(3, 1);
+    new.swap(6, 2);
+    new.swap(5, 7);
+
+    new
+}
+
+// TODO: consolidate functions into one
+fn one_away_x(line: &[Slot; 3]) -> bool {
+    use Slot::{Empty, X};
+
+    matches!(line, [X, X, Empty] | [Empty, X, X] | [X, Empty, X])
+}
+
+fn one_away_o(line: &[Slot; 3]) -> bool {
+    use Slot::{Empty, O};
+
+    matches!(line, [O, O, Empty] | [Empty, O, O] | [O, Empty, O])
+}
+
 // Takes a `Board` and returns a "score" for how good it is for `X`
 // `side` must not be `Slot::Empty`
-pub fn score(board: Board, side: Slot) -> Option<i32> {
-    use Slot::{O, X, Empty};
-
-    if side == Slot::Empty { return None }
+pub fn score(board: Board) -> Option<i32> {
     let mut score = 0;
 
-    // check rows
     for row in board.windows(3) {
-        if matches!(row, [X, X, Empty] | [Empty, X, X] | [X, Empty, X]) {
+        let row = row.try_into().unwrap();
+        if one_away_x(row) {
             score = i32::MAX;
         }
 
-        if matches!(row, [O, O, Empty] | [Empty, O, O] | [O, Empty, O]) {
+        if one_away_o(row) {
+            score = i32::MIN;
+        }
+    }
+
+    for column in tranpose(&board).windows(3) {
+        let column = column.try_into().unwrap();
+
+        if one_away_x(column) {
+            score = i32::MAX;
+        }
+
+        if one_away_o(column) {
+            score = i32::MIN;
+        }
+    }
+
+    for diag in [
+        [board[0], board[4], board[8]],
+        [board[2], board[4], board[6]],
+    ] {
+        if one_away_x(&diag) {
+            score = i32::MAX;
+        }
+
+        if one_away_o(&diag) {
             score = i32::MIN;
         }
     }
 
     Some(score)
+}
+
+pub fn full(board: &Board) -> bool {
+    board.iter().all(|sl| *sl != Slot::Empty)
 }
 
 pub struct Game {
@@ -52,29 +103,31 @@ pub struct Game {
     pub active: usize,
 }
 
-const TEMPLATE: &[u8] = br#"
-  x | x | x   |   x | x | x   |   x | x | x   
- -----------  |  -----------  |  -----------  
-  x | x | x   |   x | x | x   |   x | x | x   
- -----------  |  -----------  |  -----------  
-  x | x | x   |   x | x | x   |   x | x | x   
+/// Template for board, a-i represents which board
+/// x means insert fmt char, y means insert reset char
+const TEMPLATE: &[u8] = b"
+  \x1b[0;3nma | a | a\x1b[0m   |   \x1b[0;3omb | b | b\x1b[0m   |   \x1b[0;3pmc | c | c\x1b[0m   
+ \x1b[0;3nm-----------\x1b[0m  |  \x1b[0;3om-----------\x1b[0m  |  \x1b[0;3pm-----------\x1b[0m  
+  \x1b[0;3nma | a | a\x1b[0m   |   \x1b[0;3omb | b | b\x1b[0m   |   \x1b[0;3pmc | c | c\x1b[0m   
+ \x1b[0;3nm-----------\x1b[0m  |  \x1b[0;3om-----------\x1b[0m  |  \x1b[0;3pm-----------\x1b[0m  
+  \x1b[0;3nma | a | a\x1b[0m   |   \x1b[0;3omb | b | b\x1b[0m   |   \x1b[0;3pmc | c | c\x1b[0m   
               |               |               
 ---------------------------------------------
               |               |
-  x | x | x   |   x | x | x   |   x | x | x   
- -----------  |  -----------  |  -----------  
-  x | x | x   |   x | x | x   |   x | x | x   
- -----------  |  -----------  |  -----------  
-  x | x | x   |   x | x | x   |   x | x | x   
+  \x1b[0;3qmd | d | d\x1b[0m   |   \x1b[0;3rme | e | e\x1b[0m   |   \x1b[0;3smf | f | f\x1b[0m   
+ \x1b[0;3qm-----------\x1b[0m  |  \x1b[0;3rm-----------\x1b[0m  |  \x1b[0;3sm-----------\x1b[0m  
+  \x1b[0;3qmd | d | d\x1b[0m   |   \x1b[0;3rme | e | e\x1b[0m   |   \x1b[0;3smf | f | f\x1b[0m   
+ \x1b[0;3qm-----------\x1b[0m  |  \x1b[0;3rm-----------\x1b[0m  |  \x1b[0;3sm-----------\x1b[0m  
+  \x1b[0;3qmd | d | d\x1b[0m   |   \x1b[0;3rme | e | e\x1b[0m   |   \x1b[0;3smf | f | f\x1b[0m   
               |               |               
 ---------------------------------------------
               |               |
-  x | x | x   |   x | x | x   |   x | x | x   
- -----------  |  -----------  |  -----------  
-  x | x | x   |   x | x | x   |   x | x | x   
- -----------  |  -----------  |  -----------  
-  x | x | x   |   x | x | x   |   x | x | x   
-"#;
+  \x1b[0;3tmg | g | g\x1b[0m   |   \x1b[0;3umh | h | h\x1b[0m   |   \x1b[0;3vmi | i | i\x1b[0m   
+ \x1b[0;3tm-----------\x1b[0m  |  \x1b[0;3um-----------\x1b[0m  |  \x1b[0;3vm-----------\x1b[0m  
+  \x1b[0;3tmg | g | g\x1b[0m   |   \x1b[0;3umh | h | h\x1b[0m   |   \x1b[0;3vmi | i | i\x1b[0m   
+ \x1b[0;3tm-----------\x1b[0m  |  \x1b[0;3um-----------\x1b[0m  |  \x1b[0;3vm-----------\x1b[0m  
+  \x1b[0;3tmg | g | g\x1b[0m   |   \x1b[0;3umh | h | h\x1b[0m   |   \x1b[0;3vmi | i | i\x1b[0m   
+";
 
 impl Game {
     pub fn new() -> Self {
@@ -87,23 +140,33 @@ impl Game {
     pub fn print(&self) -> String {
         // SAFETY: arrays are guaranteed to be contiguous in memory so this should be fine
         // (also it passes miri)
-        let flat: [Slot; 81] = unsafe { std::mem::transmute(self.boards) };
+        // let flat: [Slot; 81] = unsafe { std::mem::transmute(self.boards) };
         let mut res = TEMPLATE.to_vec();
-        let mut idx = 0;
+        let mut idxs = [0; 9];
 
         for byte in res.iter_mut() {
-            if *byte == b'x' {
-                *byte = match flat[idx] {
-                    Slot::O => b'O',
-                    Slot::X => b'X',
-                    Slot::Empty => b' ',
-                };
+            if byte.is_ascii_alphabetic() {
+                match byte {
+                    b'x' => *byte = b'4',
+                    b'n'..=b'v' => {
+                        // *byte = *byte - b'n' + b'1';
+                        if (*byte - b'n') as usize == self.active || self.active == 9 {
+                            *byte = b'5';
+                        } else {
+                            *byte = b'7';
+                        }
+                    }
+                    b'm' => continue,
+                    _ => {
+                        let idx = (*byte - b'a') as usize;
+                        *byte = self.boards[idx][idxs[idx]].to_chr() as u8;
 
-                idx += 1;
+                        idxs[idx] += 1;
+                    }
+                }
             }
         }
-        
+
         String::from_utf8(res).unwrap()
     }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,89 @@
 mod board;
+use rand::Rng;
+use std::{io::Write, time::Duration};
 
-use std::io::Write;
+use board::{Game, Slot, full};
 
-use board::{Board, Slot, Game};
+fn parse_move(movestr: &str) -> Result<(usize, usize), &'static str> {
+    if movestr.len() != 2 {
+        return Err("Move string too long");
+    }
 
-fn parse_move(move: &str) -> (usize, usize) {
+    // Purposefully Invalid sentinel
+    let mut mov = (9, 9);
+    let [row, col] = movestr.as_bytes() else {
+        unreachable!()
+    };
 
+    if ('a'..='i').contains(&char::from(*row).to_ascii_lowercase()) {
+        mov.0 = *row as usize - 'a' as usize;
+    }
+
+    if char::from(*col).is_ascii_digit() {
+        mov.1 = *col as usize - '0' as usize - 1;
+    }
+
+    if mov.0 == 9 || mov.1 == 9 {
+        return Err("invalid row or column");
+    }
+
+    Ok(mov)
+}
+
+fn redraw(game: &Game) {
+    print!("\x1B[2J\x1B[1;1H");
+    println!("{}", game.print());
 }
 
 fn main() {
     let mut game = Game::new();
+    let mut mov = String::new();
+    let mut rng = rand::rng();
 
     loop {
-        println!("{}", game.print());
+        redraw(&game);
 
-        print!("Enter your move (ex. A5): ");
+        print!(
+            "Enter your move (ex. a5, active board: {}): ",
+            if game.active == 9 {
+                ' '
+            } else {
+                (game.active as u8 + b'a') as char
+            }
+        );
         std::io::stdout().flush().unwrap();
 
-        let mut user_move = String::new();
-        std::io::stdin().read_line(&mut user_move).unwrap();        
-        
-        print!("\x1B[2J\x1B[1;1H");
+        mov.clear();
+        std::io::stdin().read_line(&mut mov).unwrap();
+
+        let (game_num, idx) = parse_move(mov.trim()).unwrap();
+        if game.active == 9 {
+            game.active = game_num;
+        }
+
+        if game.active != game_num {
+            println!("must play in the active board");
+            std::thread::sleep(Duration::from_secs(1));
+
+            continue;
+        }
+
+        if game.boards[game_num][idx] != Slot::Empty {
+            println!("{} is not empty", mov.trim());
+            std::thread::sleep(Duration::from_secs(1));
+
+            continue;
+        }
+
+        game.boards[game_num][idx] = Slot::O;
+        game.active = idx;
+        redraw(&game);
+
+        // Eng move:
+        let rn = rng.random_range(0..9);
+        game.boards[game.active][rn] = Slot::X;
+        game.active = rn;
+
+        std::thread::sleep(Duration::from_secs(1));
     }
 }
